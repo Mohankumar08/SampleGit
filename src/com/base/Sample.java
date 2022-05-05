b2f9b43074c798eb3063ad8d2130c46c2f8b45a5
Mohankumar08 <pmoankumar4@gmail.com>
2022-05-05 16:37:01 +0530

    B Works

--- src/com/base/Sample.java
+++ src/com/base/Sample.java
@@ -4,5 +4,10 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("~~~~~~~~");
 		System.out.println("Hello");
+		System.out.println("--------------");
+		System.out.println("Welcome Java");
+		System.out.println("Selenium");
+		System.out.println("python");
+		System.out.println("~~~~~~~~~~~~~~~~~~");
 	}
 }
